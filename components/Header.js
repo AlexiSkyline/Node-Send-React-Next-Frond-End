4f09c2cb152c39ed9f3bf630ab72c7e90743b7d3
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-12-01 17:56:33 -0600

    create our Header Component

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <header className='py-8 flex flex-col md:flex-row item-center justify-between'>
+            <Link  href='/'>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img className='w-64 mb-8 md:mb-0' src='logo.svg' alt='logo.svg'/>
+            </Link>
+
+            <div className='flex item-center'>
+                <Link href='/login'>
+                    <a className='bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2'>
+                        Iniciar Sesión
+                    </a>
+                </Link>
+
+                <Link href='/crearcuenta'>
+                    <a className='bg-black px-5 py-3 rounded-lg text-white font-bold uppercase'>
+                        Crear Cuenta
+                    </a>
+                </Link>
+            </div>
+        </header>
+
+    );
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Header from './Header';
 
 export default function Layout ({ children }) {
     return (
@@ -8,9 +9,11 @@ export default function Layout ({ children }) {
                 <title>React NodeSend</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"/>
             </Head>
-            
+
             <div className='bg-gray-100 min-h-screen'>
                 <div className='container mx-auto'>
+                    <Header />
+                    
                     <main className='mt-20'>
                         { children }
                     </main>
