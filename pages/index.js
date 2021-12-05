cdf5a1c41650b313595155a3f1dc282564f0950b
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-12-04 21:13:19 -0600

    we show the link to share

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
     // Todo: Extrae el mensaje del archivo
     const AppContext = useContext( appContext );
-    const { mensajeArchivo } = AppContext;
+    const { mensajeArchivo, url } = AppContext;
 
     useEffect(() => {
         usuarioAutenticado();
@@ -24,28 +24,54 @@ export default function Home() {
             <div className='md:w-4/5 xl:w-3/5 mx-auto mb-32'>
 
                 {
-                    mensajeArchivo && <Alerta />
+                    url ? 
+                    (
+                        <>
+                            <p className='text-center text-2xl'>
+                                <span className='font-bold text-red-700 text-3xl uppercase'>
+                                    Tu URL es:
+                                </span> 
+                                { `${ process.env.frontendURL }/enlaces/${ url }` }
+                            </p>
+
+                            <button
+                                type='button'
+                                className='bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold mt-8'
+                                onClick={ () => navigator.clipboard.writeText( `${ process.env.frontendURL }/enlaces/${ url }` ) }
+                            >
+                                Copiar Enlace
+                            </button>
+                        </>
+                    )
+                    :
+                    (
+                        <>
+                            {
+                                mensajeArchivo && <Alerta />
+                            }
+
+                            <div className='lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10'>
+                                <Dropzone />
+
+                                <div className='md:flex-1 mb-3 mx-2 mt-16 lg:mt-0'>
+                                    <h2 className='text-4xl font-sans font-bold text-gray-800 my-4'>
+                                        Compartir archivos de formato sencilla y privada
+                                    </h2>
+
+                                    <p className='text-lg leading-loose'>
+                                        <span className='text-red-500 font-bold'>ReactNodeSend</span> te permite compartir archivos con cifrado de 
+                                        extremo a extremo y un arcivo que es eliinado después de ser descargadp. Así que puedes mantener lo que 
+                                        compartes en privado y asegurarte de que tus cosas no permanezcan en línea para siempre
+                                    </p>
+
+                                    <Link href='/_crearcuenta'>
+                                        <a className='text-red-500 font-bold text-lg hover:text-red-700'>Crea una cuenta para mayores baneficios</a>
+                                    </Link>
+                                </div>
+                            </div>
+                        </>
+                    )
                 }
-
-                <div className='lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10'>
-                    <Dropzone />
-
-                    <div className='md:flex-1 mb-3 mx-2 mt-16 lg:mt-0'>
-                        <h2 className='text-4xl font-sans font-bold text-gray-800 my-4'>
-                            Compartir archivos de formato sencilla y privada
-                        </h2>
-
-                        <p className='text-lg leading-loose'>
-                            <span className='text-red-500 font-bold'>ReactNodeSend</span> te permite compartir archivos con cifrado de 
-                            extremo a extremo y un arcivo que es eliinado después de ser descargadp. Así que puedes mantener lo que 
-                            compartes en privado y asegurarte de que tus cosas no permanezcan en línea para siempre
-                        </p>
-
-                        <Link href='/_crearcuenta'>
-                            <a className='text-red-500 font-bold text-lg hover:text-red-700'>Crea una cuenta para mayores baneficios</a>
-                        </Link>
-                    </div>
-                </div>
             </div> 
         </Layout>
     );
