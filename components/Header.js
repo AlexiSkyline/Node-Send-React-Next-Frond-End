2dd940d697e3464608699db106f242f464243e80
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-12-05 20:40:39 -0600

    add the page and the function for downloading files

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ export default function Header() {
         <header className='py-8 flex flex-col md:flex-row item-center justify-between'>
             <Link href='/' passHref>
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img className='w-64 mb-8 md:mb-0 cursor-pointer' src='logo.svg' alt='logo.svg'/>
+                <img className='w-64 mb-8 md:mb-0 cursor-pointer' src='/logo.svg' alt='logo.svg'/>
             </Link>
 
             <div className='flex item-center'>
